8c2c9e2b5286968f2653e43c3e459b58d1577c76
Andres <rubioloandres@gmail.com>
2020-06-18 10:55:57 -0300

    Nuevo servicio, CXF_3

--- DAS/FinalDAS/project/workspace/indec_service/indec_service_db/src/main/resources/liquibase/populates/cadenaServiceConfig.sql
+++ DAS/FinalDAS/project/workspace/indec_service/indec_service_db/src/main/resources/liquibase/populates/cadenaServiceConfig.sql
@@ -5,6 +5,7 @@ VALUES
     , (3,5,2,'http://localhost:8001/cadena_rest_one/cadenaRestOne')               --  'Disco',
     , (4,2,2,'http://localhost:8002/cadena_rest_two/cadenaRestTwo') --Jumbo
     , (5,3,1,'http://localhost:8004/cadena_cxf_two/services/cadena_cxf_two?wsdl') -- Carrefour
-    , (6,6,2,'http://localhost:8010/cadena_rest_three/cadenaRestThree') -- EXTRA - SUPERMAMI2 - REST3
---    , (6,6,1,'http://localhost:8000/cadena_axis_two/services/CadenaAxisTwo?wsdl') -- EXTRA - SUPERMAMI - AXIS2
+    , (6,6,1,'http://localhost:8010/cadena_cxf_three/services/cadena_cxf_three?wsdl') -- EXTRA - SUPERMAMI3 - CXF3
+--    , (6,6,2,'http://localhost:8010/cadena_rest_three/cadenaRestThree') -- EXTRA - SUPERMAMI2 - REST3
+--    , (6,6,1,'http://localhost:8010/cadena_axis_two/services/CadenaAxisTwo?wsdl') -- EXTRA - SUPERMAMI - AXIS2
 
